3cc784c124af8eb555aaf7054b712c8b96c97091
Rui Lopes <rui.d.lopes@me.com>
2025-02-27 21:00:52 +0100

    refactor: clean code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test-ui-shell
+
 A web components test using IBM Carbon UI
---
+++ src/ui-shell.ts
@@ -0,0 +1,68 @@
+import {LitElement, css, html, unsafeCSS} from 'lit'
+import {customElement, query} from 'lit/decorators.js'
+import styles from "./ui-shell.scss?inline"
+
+import '@carbon/web-components/es/components/ui-shell/index.js';
+import Switcher20 from '@carbon/web-components/es/icons/switcher/20.js';
+import {CDSHeaderGlobalAction, CDSHeaderPanel} from "@carbon/web-components/es";
+
+@customElement('ui-shell')
+export class UIShell extends LitElement {
+    static readonly styles = css`${unsafeCSS(styles)}`
+
+    @query('#switcher-panel')
+    private readonly _switcherPanel!: CDSHeaderPanel;
+
+    @query('cds-header-global-action')
+    private readonly _globalAction!: CDSHeaderGlobalAction;
+
+    render() {
+        return html`
+            <header>
+                <cds-header aria-label="UI Shell">
+                    <cds-header-name
+                            href="javascript:void 0"
+                            prefix="UI"
+                    >Shell
+                    </cds-header-name>
+                    <div class="cds--header__global">
+                        <cds-header-global-action
+                                button-label-active="Close switcher"
+                                button-label-inactive="Open switcher"
+                                tooltip-text="Open switcher"
+                                panel-id="switcher-panel"
+                                tooltip-alignment="right"
+                                @click="${() => {
+                                    this._globalAction.active = !this._globalAction.active;
+                                    this._switcherPanel.expanded = !this._switcherPanel.expanded
+                                }}"
+                        >${Switcher20({slot: 'icon'})}
+                        </cds-header-global-action>
+                    </div>
+                    <cds-header-panel
+                            id="switcher-panel"
+                            aria-label="App switcher Panel"
+                    >App switcher Panel
+                    </cds-header-panel>
+                </cds-header>
+            </header>
+            
+            <main class="cds--content">
+                <div class="cds--grid">
+                    <div class="cds--row">
+                        <div class="cds--column">
+                            <slot name="content"></slot>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        `
+    }
+
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'ui-shell': UIShell
+    }
+}
